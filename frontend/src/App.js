6ed3909ea6f91031b4758386eda729d72fd2d3df
Heero Punjabi <heeropunjabi@gmail.com>
2023-07-10 07:40:11 +0530

    frontend changes

---
+++ frontend/src/App.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Layout, Space, ConfigProvider } from 'antd';
+import CustomContent from './CustomContent';
+const { Header, Footer, Sider, Content } = Layout;
+
+
+
+const headerStyle = {
+  textAlign: 'center',
+  color: 'black',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: 'rgb(245, 245, 245)',
+  borderBottom: '1px solid rgba(0, 21, 41, 0.14)',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center'
+};
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: '85vh',
+  
+  color: 'black',
+  backgroundColor: 'white',
+};
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+};
+const footerStyle = {
+  textAlign: 'center',
+  color: 'black',
+  backgroundColor: 'white',
+  borderTop: '1px solid rgba(0, 21, 41, 0.14)',
+};
+
+
+function App() {
+  const [locale, setLocale] = useState('en');
+
+
+  
+
+  return (
+    <ConfigProvider locale={locale}>
+    <Layout>
+      <Header style={headerStyle}>
+        <div>
+          <img src="https://www.thesparksfoundationsingapore.org/images/logo_small.png" alt="logo" style={{
+            position: 'absolute',
+            left: '100px',
+            top: '7px',
+            width: '75px',
+            height: '50px',
+          }} />
+          
+
+        </div>
+      </Header>
+      <Content style={contentStyle}>
+          <CustomContent locale={locale} />
+      </Content>
+      <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+      </ConfigProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/CustomContent.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+
+export default function CustomContent() {
+    return (
+        <div>
+            <h3>Content</h3>
+            <h4>Content</h4>
+            <p>Content</p>
+        </div>
+    )
+}
